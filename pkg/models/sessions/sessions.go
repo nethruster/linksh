ab5db24e12065b530baf702fee554bb97e17d8e6
Claudio4 <me@claudio4.com>
2018-08-06 00:12:21 +0100

    Add session encode

--- pkg/models/sessions/sessions.go
+++ pkg/models/sessions/sessions.go
@@ -1,11 +1,20 @@
 package sessions
 
+import (
+	"fmt"
+)
+
 //Session is the datatype which contains all the relevant information about a session
 type Session struct {
 	ID        string
 	OwnerID   string
 	ExpiresOn int64
 	CreatedAt int64
+}
+
+//Encode return a string with the basic session data in the spected format for the its usage as a auth method
+func (session *Session) Encode() string {
+	return fmt.Sprintf("%v%v", session.ID, session.OwnerID)
 }
 
 //SessionProvider is the interface for a valid session storage
