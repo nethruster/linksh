0c29b5105d043294e03cf069bd27238079d6e231
Claudio Yanes <me@claudio4.com>
2019-07-15 03:00:48 +0100

    Fix typo

--- pkg/interfaces/IUserRepository.go
+++ pkg/interfaces/IUserRepository.go
@@ -7,10 +7,20 @@ import (
 //IUserRepository represents all the possible actions performed over the users
 //The implementations of this interface will not be attached to an specific storage
 type IUserRepository interface {
+	//CheckLoginCredentials checks if the provided credentials are valid to perform a login
 	CheckLoginCredentials(username, password string) (bool, error)
+	//Create creates an user and save it to the storage
+	//This methods will permorn validations over the provided data
 	Create(username, password string) (models.User, error)
+	//Get returns an user from the storage
 	Get(id string) (models.User, error)
+	//List lits the users
+	//If limit is set to 0, no limit will be established
 	List(limit, offset uint) ([]models.User, error)
+	//Update replaces the values of the user in the storage with the values of the user provided by parameter
+	//If the user doesn't exists in the storage an error will be returned
+	//This methods will permorn validations over the provided data
 	Update(user models.User) error
-	Delete(userID string) error
+	//Delete deletes an user from the storage
+	Delete(id string) error
 }
