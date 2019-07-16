e81d96d7cfeae22dc80a76532a3d6330e15f9a93
Claudio Yanes <me@claudio4.com>
2019-07-16 03:58:28 +0100

    Add constrains to the models
    Informs which constrains are required for certain field
    in the form of an annotation, although it just for the sake of documentation

--- pkg/models/User.go
+++ pkg/models/User.go
@@ -2,7 +2,7 @@ package models
 
 //User represents a user in the core logic
 type User struct {
-	ID       string `json:"id"`
-	Name     string `json:"name"`
+	ID       string `json:"id"`   //must be unique
+	Name     string `json:"name"` //must be unique and no longer that 100 characters
 	Password []byte `json:"-"`
 }
