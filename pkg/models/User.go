4c5a47acf0bd0347bf5cb8587b814cbe66f4a5eb
Claudio Yanes <me@claudio4.com>
2019-07-16 20:13:27 +0100

    Set a minimum length to the passwords

--- pkg/models/User.go
+++ pkg/models/User.go
@@ -4,6 +4,6 @@ package models
 type User struct {
 	ID       string `json:"id"`   //must be unique
 	Name     string `json:"name"` //must be unique and no longer that 100 characters
-	Password []byte `json:"-"`
+	Password []byte `json:"-"`    //must be atleast than six characters long
 	IsAdmin  bool   `json:"isAdmin"`
 }
