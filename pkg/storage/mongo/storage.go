98bef99784e18f60464e7d4e6e393ff1680f5d45
Claudio Yanes <me@claudio4.com>
2020-03-23 18:49:39 +0000

    Create MongoStorage

---
+++ pkg/storage/mongo/storage.go
@@ -0,0 +1,83 @@
+package mongo
+
+type MongoStorage struct {
+}
+
+//User related methods
+
+//SaveUser save the user in the storage
+//If there is a conflicting unique field this method will return an AlreadyExistsError
+func (sto *MongoStorage) SaveUser(user models.User) error {
+
+}
+
+//GetUser returns the user with specified ID from the storage
+//If the user does not exists in the storage an NotFoundError would be returned
+func (sto *MongoStorage) GetUser(id string) (models.User, error) {
+
+}
+
+//GetUserByName returns the user with specified name from the storage
+//If the user does not exists in the storage an NotFoundError would be returned
+func (sto *MongoStorage) GetUserByName(name string) (models.User, error) {
+
+}
+
+//ListUsers list the users in the storage with a limit and an offset
+//If the limit is set to 0, no limit will be established, the same applies to the offset
+func (sto *MongoStorage) ListUsers(limit, offset uint) ([]models.User, error) {
+
+}
+
+//UpdateUser replaces the values of the user in the storage with the non empty ones of the provided user
+//If the user does not exists in the storage a NotFoundError would be returned
+//If there is a conflicting unique field this method will return an AlreadyExistsError
+func (sto *MongoStorage) UpdateUser(user user_repository.UpdatePayload) error {
+
+}
+
+//DeleteUser deletes the user specified user from the storage
+//If the user does not exists in the storage an NotFoundError would be returned
+func (sto *MongoStorage) DeleteUser(id string) error {
+
+}
+
+//Link related methods
+
+//SaveLink save the link in the storage
+//If there is a conflicting unique field this method will return an AlreadyExistsError
+func (sto *MongoStorage) SaveLink(link models.Link) error {
+
+}
+
+//GetLink returns the link with specified ID from the storage
+//If the link does not exists in the storage an NotFoundError would be returned
+func (sto *MongoStorage) GetLink(id string) (models.Link, error) {
+
+}
+
+//ListLinks list the links in the storage with a limit and an offset
+//if the ownerID is not empty the search would be limited to the ones owned by the specified user
+//If the limit is set to 0, no limit will be established, the same applies to the offset
+func (sto *MongoStorage) ListLinks(ownerID string, limit, offset uint) ([]models.Link, error) {
+
+}
+
+//UpdateUser replaces the values of the user in the storage with the non empty ones of the provided user
+//If the link does not exists in the storage an NotFoundError would be returned
+//If there is a conflicting unique field this method will return an AlreadyExistsError
+func (sto *MongoStorage) UpdateLinkContent(id, content string) error {
+
+}
+
+//DeleteLink deletes the link specified user from the storage
+//If the link does not exists in the storage an NotFoundError would be returned
+func (sto *MongoStorage) DeleteLink(id string) error {
+
+}
+
+//IncreaseLinkHitCount increases the hits number of a link in the storage
+//If the user does not exists in the storage an NotFoundError would be returned
+func (sto *MongoStorage) IncreaseLinkHitCount(id string) error {
+
+}
