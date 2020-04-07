e576d7370fc2efe6e195b697d052ac0fbe60f8aa
Claudio Yanes <me@claudio4.com>
2020-04-07 17:00:53 +0100

    Put comments over field in models/session

--- pkg/models/session.go
+++ pkg/models/session.go
@@ -1,10 +1,10 @@
 package models
 
 //Session describes a session
-//ExpireDate is an Unix EPOCH
 type Session struct {
 	ID         string `json:"id" bson:"_id"`
 	UserID     string `json:"user_id" bson:"user_id"`
 	LastToken  string `json:"last_token" bson:"last_token"`
+	//ExpireDate must be an Unix EPOCH
 	ExpireDate uint64 `json:"expire_date" bson:"expire_date"`
 }
