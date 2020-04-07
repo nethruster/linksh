6e7a8af431d5fa47cd19d8989f9c29c8d2f20c79
Claudio Yanes <me@claudio4.com>
2020-04-07 17:01:58 +0100

    Add CreatedAt field to pkg/models/Link

--- pkg/models/link.go
+++ pkg/models/link.go
@@ -2,8 +2,12 @@ package models
 
 //Link represents a link in the core logic
 type Link struct {
-	ID      string `json:"id" bson:"_id"`          //must be unique and no longer that 100 characters
-	Content string `json:"content" bson:"content"` //must be no longer that 2000 characters
-	Hits    uint   `json:"hits" bson:"hits"`
-	OwnerID string `json:"ownerId" bson:"ownerId"`
+	//ID must be unique and no longer that 100 characters
+	ID      string   `json:"id" bson:"_id"`
+	//Content must be no longer that 2000 characters
+	Content string   `json:"content" bson:"content"`
+	Hits    uint     `json:"hits" bson:"hits"`
+	//CreatedAt must be an Unix EPOCH
+	CreatedAt uint64 `json:"createdAt" bson:"createdAt"`
+	OwnerID string    `json:"ownerId" bson:"ownerId"`
 }
