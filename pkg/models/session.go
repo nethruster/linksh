0f3aead79e10392418636d84e1d918b3a61d1f72
Claudio Yanes <me@claudio4.com>
2020-04-09 19:40:36 +0100

    Add CreatedAt field to Session

--- pkg/models/session.go
+++ pkg/models/session.go
@@ -5,6 +5,8 @@ type Session struct {
 	ID         string `json:"id" bson:"_id"`
 	UserID     string `json:"user_id" bson:"user_id"`
 	LastToken  string `json:"last_token" bson:"last_token"`
+	// CreatedAt must be an Unix EPOCH
+	CreatedAt int64 `json:"createdAt" bson:"createdAt"`
 	//ExpireDate must be an Unix EPOCH
-	ExpireDate uint64 `json:"expire_date" bson:"expire_date"`
+	ExpireDate int64 `json:"expire_date" bson:"expire_date"`
 }
